0f777550c77c5c1a6316b7c0ee43e9af0b6b1fde
Trent Steenholdt <trent.steenholdt@cortanadesign.com.au>
2019-02-27 10:38:13 +0800

    aa

---
+++ empty.ps1
@@ -0,0 +1,5 @@
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+Connect-AzureRmAccount -ServicePrincipal -Tenant $connection.TenantID `
+-ApplicationID $connection.ApplicationID -CertificateThumbprint $connection.CertificateThumbprint
+
+$AzureContext = Select-AzureRmSubscription -SubscriptionId $connection.SubscriptionID
